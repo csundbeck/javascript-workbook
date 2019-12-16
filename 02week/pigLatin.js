5f5d419c0064c5b6f64cbd689869f44faf1f1965
csundbeck <csundbeck04@gmail.com>
2019-12-15 18:16:50 -0600

    pigLatin Project complete

--- 02week/pigLatin.js
+++ 02week/pigLatin.js
@@ -7,11 +7,50 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
-
+//indexOf()
+//subString(startIndex, endIndexExcluded)
+//startsWith()
+//if word starts with a vowel, add 'yay' to the end. (egg becomes eggyay)
+//If word has a vowel in the middle, split the word in two at the middle, swap the parts, and add 'ay' to the end (cat becomes atcay)
+//how to solve if a word has no vowel
 function pigLatin(word) {
+  console.log('The original word is', word);
+  word = word.trim();
+  word = word.toLowerCase();
+  if (word.indexOf('a') === 0 || word.indexOf('e') === 0 || word.indexOf('i') === 0 || word.indexOf('o') === 0 || word.indexOf('u') === 0) {
+    let newWord = word + 'yay';
+    return newWord;
+  }
 
-  // Your code here
 
+  for (let i = 1; i < word.length; i++) {
+    if (word[i] === 'a') {
+      let firstSub = word.substring(0, word.indexOf('a'));
+      let secondSub = word.substring(word.indexOf('a'));
+      let newWord = secondSub + firstSub + 'ay';
+      return newWord;
+    } else if (word[i] === 'e') {
+      let firstSub = word.substring(0, word.indexOf('e'));
+      let secondSub = word.substring(word.indexOf('e'));
+      let newWord = secondSub + firstSub + 'ay';
+      return newWord;
+    } else if (word[i] === 'i') {
+      let firstSub = word.substring(0, word.indexOf('i'));
+      let secondSub = word.substring(word.indexOf('i'));
+      let newWord = secondSub + firstSub + 'ay';
+      return newWord;
+    } else if (word[i] === 'o') {
+      let firstSub = word.substring(0, word.indexOf('o'));
+      let secondSub = word.substring(word.indexOf('o'));
+      let newWord = secondSub + firstSub + 'ay';
+      return newWord;
+    } else if (word[i] === 'u') {
+      let firstSub = word.substring(0, word.indexOf('u'));
+      let secondSub = word.substring(word.indexOf('u'));
+      let newWord = secondSub + firstSub + 'ay';
+      return newWord;
+    }
+  }
 }
 
 
