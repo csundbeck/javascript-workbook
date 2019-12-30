5504c4280a0f05126c35d7823d85cd2ab7e14d7f
csundbeck <csundbeck04@gmail.com>
2019-12-29 20:15:25 -0600

    Finished Towers of Hanoi with proper rules

--- 03week/towersOfHanoi.js
+++ 03week/towersOfHanoi.js
@@ -19,24 +19,61 @@ function printStacks() {
   console.log("c: " + stacks.c);
 }
 
-function movePiece() {
-  // Your code here
-
+//move the top (last iitem in the array) piece from the startstack to the top of the endstack
+//when deciding where to move pieces, use stacks[startStack] and stacks[endStack]
+function movePiece(startStack, endStack) {
+  let itemToMove = stacks[startStack].pop();
+  stacks[endStack].push(itemToMove);
 }
 
-function isLegal() {
-  // Your code here
+//return true if the move is legal
+//return false if the move is illegal
+//legal move if:
+//the startstack has at least 1 piece && (endstack is empty || when top piece of startstack is less than top piece of end stack)
+function isLegal(startStack, endStack) {
+  let lengthOfStart = stacks[startStack].length;
+  let lengthOfEnd = stacks[endStack].length;
 
+  if (lengthOfStart != 0 && (lengthOfEnd == 0 || (stacks[endStack][lengthOfEnd-1] > stacks[startStack][lengthOfStart-1]))) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
+//Return true if player won
+//Return false if player did not win
+//a win is when all 4 blocks are in either stack b or stack c 
 function checkForWin() {
-  // Your code here
-
+  if (stacks["b"].length === 4) {
+    return true;
+  } else if (stacks["c"].length === 4) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
+//takes in user input for tartstack and endstack and digitally plays the round
 function towersOfHanoi(startStack, endStack) {
   // Your code here
-
+  //check if the move is legal
+  //if move is legal
+  //   -move the piece
+  //   -check if they won (tell them that they won)
+  //else If move is not legal
+  //   -tell them the move is not legal
+  //check to see if they won
+  //    -tell them they won!
+  if (isLegal(startStack, endStack) === true) {
+    movePiece(startStack, endStack);
+    if (checkForWin() === true) {
+      console.log("You win!!!");
+    }
+  } else {
+    console.log('That move is not legal. Please try again...');
+  }
+  // checkForWin();
 }
 
 function getPrompt() {
@@ -44,7 +81,11 @@ function getPrompt() {
   rl.question('start stack: ', (startStack) => {
     rl.question('end stack: ', (endStack) => {
       towersOfHanoi(startStack, endStack);
-      getPrompt();
+      if (checkForWin() === true) {
+        return true;
+      } else {
+        getPrompt();
+      }
     });
   });
 }
